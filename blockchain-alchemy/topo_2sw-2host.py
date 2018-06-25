f55ca9ea2b01a629fac8a90da82e92d778f8ac0d
Petr <giggsoff@gmail.com>
2018-06-25 13:02:05 +0300

    Add "raw" variant without encryption

--- blockchain-alchemy/topo_2sw-2host.py
+++ blockchain-alchemy/topo_2sw-2host.py
@@ -1,5 +1,6 @@
 from mininet.topo import Topo
 from mininet.link import Link
+from mininet.link import TCIntf
 from mininet import util
 from mininet.node import OVSSwitch
 import time
@@ -12,19 +13,24 @@ class QKLink(Link):
     @classmethod
     def makeIntfPair(cls, intfname1, intfname2, addr1=None, addr2=None,
                      node1=None, node2=None, deleteIntfs=True):
-        """Create pair of interfaces
-           intfname1: name for interface 1
-           intfname2: name for interface 2
-           addr1: MAC address for interface 1 (optional)
-           addr2: MAC address for interface 2 (optional)
-           node1: home node for interface 1 (optional)
-           node2: home node for interface 2 (optional)
-           (override this method [and possibly delete()]
-           to change link type)"""
-        # Leave this as a class method for now
         assert cls
+        print('QKlink')
         return makeIntfPair(intfname1, intfname2, addr1, addr2, node1, node2,
-                            deleteIntfs)
+                            deleteIntfs, isEncrypted=True)
+
+    def stop(self):
+        for p in QKLink.processes:
+            p.kill()
+        self.delete()
+
+class QKLinkRaw(Link):
+    @classmethod
+    def makeIntfPair(cls, intfname1, intfname2, addr1=None, addr2=None,
+                     node1=None, node2=None, deleteIntfs=True):
+        assert cls
+        print('QKlinkRaw')
+        return makeIntfPair(intfname1, intfname2, addr1, addr2, node1, node2,
+                            deleteIntfs, isEncrypted=False)
 
     def stop(self):
         for p in QKLink.processes:
@@ -36,17 +42,7 @@ QKLink.processes = []
 
 
 def makeIntfPair(intf1, intf2, addr1=None, addr2=None, node1=None, node2=None,
-                 deleteIntfs=True, runCmd=None):
-    """Make a veth pair connnecting new interfaces intf1 and intf2
-       intf1: name for interface 1
-       intf2: name for interface 2
-       addr1: MAC address for interface 1 (optional)
-       addr2: MAC address for interface 2 (optional)
-       node1: home node for interface 1 (optional)
-       node2: home node for interface 2 (optional)
-       deleteIntfs: delete intfs before creating them
-       runCmd: function to run shell commands (quietRun)
-       raises Exception on failure"""
+                 deleteIntfs=True, runCmd=None, isEncrypted=True):
     if not runCmd:
         runCmd = util.quietRun if not node1 else node1.cmd
         runCmd2 = util.quietRun if not node2 else node2.cmd
@@ -54,35 +50,43 @@ def makeIntfPair(intf1, intf2, addr1=None, addr2=None, node1=None, node2=None,
         # Delete any old interfaces with the same names
         runCmd('ip link del ' + intf1)
         runCmd2('ip link del ' + intf2)
-    """cmdOutput = util.run('ip tuntap add %s mode tap' %
-                         (intf1))
-    if cmdOutput:
-        raise Exception("Error creating interface pair (%s,%s): %s " %
-                        (intf1, intf2, cmdOutput))
-    cmdOutput = util.run('ip tuntap add %s mode tap' %
-                         (intf2))
-    if cmdOutput:
-        raise Exception("Error creating interface pair (%s,%s): %s " %
-                        (intf1, intf2, cmdOutput))"""
+
     # Create new pair
     netns = 1 if not node2 else node2.pid
+    addEnc = '-e 100'
+    if isEncrypted == False:
+        addEnc = ''
     print(
-                '\n/root/qnet/ctapudp/ctapudp -s 0.0.0.0 -p %i -t 127.0.0.1 -k %i -i %s -a 1 -q 127.0.0.1 -r 55554 -e 100\n' % (
-            makeIntfPair.portscount, makeIntfPair.portscount + 1, intf1))
+                '\n/root/qnet/ctapudp/ctapudp -s 0.0.0.0 -p %i -t 127.0.0.1 -k %i -i %s -a 1 -q 127.0.0.1 -r 55554 %s\n' % (
+            makeIntfPair.portscount, makeIntfPair.portscount + 1, intf1, addEnc))
     print(
-                '\n/root/qnet/ctapudp/ctapudp -s 0.0.0.0 -p %i -t 127.0.0.1 -k %i -i %s -a 1 -q 127.0.0.1 -r 55554 -e 100\n' % (
-            makeIntfPair.portscount + 1, makeIntfPair.portscount, intf2))
-    process = subprocess.Popen(
-        ['/root/qnet/ctapudp/ctapudp', '-s', '127.0.0.1', '-p', str(makeIntfPair.portscount), '-t', '127.0.0.1',
-         '-k',
-         str(makeIntfPair.portscount + 1), '-i', intf1, '-a', '1', '-q', '127.0.0.1', '-r', '55554', '-e',
-         '100'""", '-d', '1'"""], preexec_fn=os.setpgrp)
+                '\n/root/qnet/ctapudp/ctapudp -s 0.0.0.0 -p %i -t 127.0.0.1 -k %i -i %s -a 1 -q 127.0.0.1 -r 55554 %s\n' % (
+            makeIntfPair.portscount + 1, makeIntfPair.portscount, intf2, addEnc))
+    if isEncrypted == True:
+        process = subprocess.Popen(
+            ['/root/qnet/ctapudp/ctapudp', '-s', '127.0.0.1', '-p', str(makeIntfPair.portscount), '-t', '127.0.0.1',
+             '-k',
+             str(makeIntfPair.portscount + 1), '-i', intf1, '-a', '1', '-q', '127.0.0.1', '-r', '55554', '-e',
+             '100'""", '-d', '1'"""], preexec_fn=os.setpgrp)
+    else:
+        process = subprocess.Popen(
+            ['/root/qnet/ctapudp/ctapudp', '-s', '127.0.0.1', '-p', str(makeIntfPair.portscount), '-t', '127.0.0.1',
+             '-k',
+             str(makeIntfPair.portscount + 1), '-i', intf1, '-a', '1', '-q', '127.0.0.1', '-r', '55554'
+             """, '-d', '1'"""], preexec_fn=os.setpgrp)
     QKLink.processes.append(process)
-    process = subprocess.Popen(
-        ['/root/qnet/ctapudp/ctapudp', '-s', '127.0.0.1', '-p', str(makeIntfPair.portscount + 1), '-t', '127.0.0.1',
-         '-k',
-         str(makeIntfPair.portscount), '-i', intf2, '-a', '1', '-q', '127.0.0.1', '-r', '55554', '-e',
-         '100'""", '-d', '1'"""], preexec_fn=os.setpgrp)
+    if isEncrypted == True:
+        process = subprocess.Popen(
+            ['/root/qnet/ctapudp/ctapudp', '-s', '127.0.0.1', '-p', str(makeIntfPair.portscount + 1), '-t', '127.0.0.1',
+             '-k',
+             str(makeIntfPair.portscount), '-i', intf2, '-a', '1', '-q', '127.0.0.1', '-r', '55554', '-e',
+             '100'""", '-d', '1'"""], preexec_fn=os.setpgrp)
+    else:
+        process = subprocess.Popen(
+            ['/root/qnet/ctapudp/ctapudp', '-s', '127.0.0.1', '-p', str(makeIntfPair.portscount + 1), '-t', '127.0.0.1',
+             '-k',
+             str(makeIntfPair.portscount), '-i', intf2, '-a', '1', '-q', '127.0.0.1', '-r', '55554'
+             """, '-d', '1'"""], preexec_fn=os.setpgrp)
     QKLink.processes.append(process)
     time.sleep(0.5)
     makeIntfPair.portscount = makeIntfPair.portscount + 2
@@ -133,13 +137,11 @@ class MyTopo(Topo):
         rightHost = self.addHost('h2')
         leftSwitch = self.addSwitch('s1', cls = OVSSwitch, protocols='OpenFlow13', dpid='000016ccbbe0d642')
         rightSwitch = self.addSwitch('s2', cls = OVSSwitch, protocols='OpenFlow13', dpid='00001ad87e868d45')
-
         # Add links
         self.addLink(leftHost, leftSwitch, cls=QKLink)
         self.addLink(rightHost, rightSwitch, cls=QKLink)
-        self.addLink(leftSwitch, rightSwitch)
+        self.addLink(leftSwitch, rightSwitch, cls=QKLinkRaw)
         self.addLink(leftSwitch, rightSwitch, cls=QKLink)
-
 
 process = subprocess.Popen(
     ['/root/qnet/keyworker/keyworker', '-n', '/root/qnet/keyworker/db1', '-w', '2'""", '-d', '1'"""],
