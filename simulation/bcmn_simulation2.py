656c28973958adde13cb3cc0472e15d118c0aff3
None <None>
2019-04-03 13:23:46 -0700

    new host_type for simulations & POS improvements

--- simulation/bcmn_simulation2.py
+++ simulation/bcmn_simulation2.py
@@ -12,25 +12,19 @@ from mininet.node import Host, CPULimitedHost, OVSKernelSwitch
 from mininet.util import specialClass
 from mininet.term import makeTerms
 from bal.bcnode import POWNode, POSNode
+import random_topology_generator as rtg
 
 def simulate(host_type):
     net = None
     try:
-        net = Mininet( topo=None, build=False, host=host_type, ipBase='10.0.0.0/8', xterms=True, waitConnected=True)
+        net = rtg.random_topology(3, 5, 100, host_type)
 
-        h1 = net.addHost('h1', ip='10.0.0.1', defaultRoute=None)
-        h2 = net.addHost('h2', ip='10.0.0.2', defaultRoute=None)
-        h3 = net.addHost('h3', ip='10.0.0.3', defaultRoute=None)
-        h4 = net.addHost('h4', ip='10.0.0.4', defaultRoute=None)
-        verifier = net.addHost('verifier', ip='10.0.1.0', defaultRoute=None)
-        s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
-        s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
-        net.addLink(s1, h1)
-        net.addLink(s1, h2)
-        net.addLink(s1, h3)
-        net.addLink(s1, h4)
-        net.addLink(s1, verifier)
-        net.addLink(s2, s1)
+        h1 = net.hosts[0]
+        h2 = net.hosts[1]
+        h3 = net.hosts[2]
+        h4 = net.hosts[3]
+        verifier =net.hosts[4]
+
         net.build()
         net.start()
 
@@ -63,83 +57,83 @@ def simulate(host_type):
 
         # 6/11/10
         open_mininet_cli(net)
-        h5 = add_host_helper('h5', '10.0.0.5', s2, net)
-        register_peers(h2, h5)
-        register_peers(h3, h5)
-
-        raw_input('Input something to start transactions in 6/11/10: ')
-        send_transaction(h2, h5, 20)
-        send_transaction(h3, h5, 35)
-        wait_and_forge_transactions(verifier, 2)
-
-        #8/11/10 Phase1
-        open_mininet_cli(net)
-        h6 = add_host_helper('h6', '10.0.0.6', s2, net)
-        h7 = add_host_helper('h7', '10.0.0.7', s2, net)
-        h8 = add_host_helper('h8', '10.0.0.8', s1, net)
-        register_peers(h4, h7)
-        register_peers(h4, h8)
-        register_peers(h5, h7)
-        register_peers(h6, h7)
-        register_peers(h5, h6)
-        register_peers(h1, h6)
-
-        raw_input('Input something to send transactions in 8/11/10 Phase1: ')
-        send_transaction(h1, h6, 12)
-        send_transaction(h4, h7, 30)
-        send_transaction(h4, h8, 15)
-        wait_and_forge_transactions(verifier, 3)
-
-        #8/11/10 between entity
-        raw_input('Input something to send mid-transactions(between entity) in 8/11/10')
-        send_transaction(h6, h5, 5)
-        send_transaction(h7, h5, 30)
-        wait_and_forge_transactions(verifier, 2)
-
-        #8/11/10 Phase2
-        open_mininet_cli(net)
-        h9 = add_host_helper('h9', '10.0.0.9', s1, net)
-        h10 = add_host_helper('h10', '10.0.0.10', s1, net)
-        register_peers(h6, h9)
-        register_peers(h5, h10)
-
-        raw_input('Input something to send transactions in 8/11/10 Phase2: ')
-        send_transaction(h6, h9, 7)
-        send_transaction(h5, h10, 90)
-        wait_and_forge_transactions(verifier, 2)
-
-        #8/11/10 Phase3
-        raw_input('Input something to send transactions in 8/11/10 Phase3: ')
-        send_transaction(h10, verifier, 90)
-        wait_and_forge_transactions(verifier, 1)
-
-        #15/06/2011
-        open_mininet_cli(net)
-        h11 = add_host_helper('h11', '10.0.0.11', s1, net)
-        register_peers(h8, h11)
-
-        raw_input('Input something to send transactions in 15/06/2011: ')
-        send_transaction(h8, h11, 15)
-        wait_and_forge_transactions(verifier, 1)
-
-        #17/06/2011: Phase1
-        open_mininet_cli(net)
-        h12 = add_host_helper('h12', '10.0.0.12', s1, net)
-        register_peers(h11, h12)
-
-        raw_input('Input something to send transactions in 17/06/2011: Phase1: ')
-        target_amount = 12
-        transaction_count = 8
-        print("Sending " + str(transaction_count) + " number of transactions from h11 to h12")
-        for i in range(1, transaction_count + 1):
-            send_transaction(h11, h12, target_amount/float(transaction_count), True)
-            wait_and_forge_transactions(verifier, 1)
-            sleep(0.5)
-
-        #17/06/2011: Phase2
-        raw_input('Input something to send transactions in 17/06/2011 Phase2: ')
-        send_transaction(h12, verifier, 12)
-        wait_and_forge_transactions(verifier, 1)
+        # h5 = add_host_helper('h5', '10.0.0.5', s2, net)
+        # register_peers(h2, h5)
+        # register_peers(h3, h5)
+        #
+        # raw_input('Input something to start transactions in 6/11/10: ')
+        # send_transaction(h2, h5, 20)
+        # send_transaction(h3, h5, 35)
+        # wait_and_forge_transactions(verifier, 2)
+        #
+        # #8/11/10 Phase1
+        # open_mininet_cli(net)
+        # h6 = add_host_helper('h6', '10.0.0.6', s2, net)
+        # h7 = add_host_helper('h7', '10.0.0.7', s2, net)
+        # h8 = add_host_helper('h8', '10.0.0.8', s1, net)
+        # register_peers(h4, h7)
+        # register_peers(h4, h8)
+        # register_peers(h5, h7)
+        # register_peers(h6, h7)
+        # register_peers(h5, h6)
+        # register_peers(h1, h6)
+        #
+        # raw_input('Input something to send transactions in 8/11/10 Phase1: ')
+        # send_transaction(h1, h6, 12)
+        # send_transaction(h4, h7, 30)
+        # send_transaction(h4, h8, 15)
+        # wait_and_forge_transactions(verifier, 3)
+        #
+        # #8/11/10 between entity
+        # raw_input('Input something to send mid-transactions(between entity) in 8/11/10')
+        # send_transaction(h6, h5, 5)
+        # send_transaction(h7, h5, 30)
+        # wait_and_forge_transactions(verifier, 2)
+        #
+        # #8/11/10 Phase2
+        # open_mininet_cli(net)
+        # h9 = add_host_helper('h9', '10.0.0.9', s1, net)
+        # h10 = add_host_helper('h10', '10.0.0.10', s1, net)
+        # register_peers(h6, h9)
+        # register_peers(h5, h10)
+        #
+        # raw_input('Input something to send transactions in 8/11/10 Phase2: ')
+        # send_transaction(h6, h9, 7)
+        # send_transaction(h5, h10, 90)
+        # wait_and_forge_transactions(verifier, 2)
+        #
+        # #8/11/10 Phase3
+        # raw_input('Input something to send transactions in 8/11/10 Phase3: ')
+        # send_transaction(h10, verifier, 90)
+        # wait_and_forge_transactions(verifier, 1)
+        #
+        # #15/06/2011
+        # open_mininet_cli(net)
+        # h11 = add_host_helper('h11', '10.0.0.11', s1, net)
+        # register_peers(h8, h11)
+        #
+        # raw_input('Input something to send transactions in 15/06/2011: ')
+        # send_transaction(h8, h11, 15)
+        # wait_and_forge_transactions(verifier, 1)
+        #
+        # #17/06/2011: Phase1
+        # open_mininet_cli(net)
+        # h12 = add_host_helper('h12', '10.0.0.12', s1, net)
+        # register_peers(h11, h12)
+        #
+        # raw_input('Input something to send transactions in 17/06/2011: Phase1: ')
+        # target_amount = 12
+        # transaction_count = 8
+        # print("Sending " + str(transaction_count) + " number of transactions from h11 to h12")
+        # for i in range(1, transaction_count + 1):
+        #     send_transaction(h11, h12, target_amount/float(transaction_count), True)
+        #     wait_and_forge_transactions(verifier, 1)
+        #     sleep(0.5)
+        #
+        # #17/06/2011: Phase2
+        # raw_input('Input something to send transactions in 17/06/2011 Phase2: ')
+        # send_transaction(h12, verifier, 12)
+        # wait_and_forge_transactions(verifier, 1)
 
     finally:
         result=CLI(net)
---
+++ simulation/random_topology_generator.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import random
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.link import Link, TCLink
+import itertools
+import sys
+
+def graph_to_str(v, adj_matrix):
+    str_matrix = []
+    for i in range (1, v):
+        for j in range(i+1, v+1):
+            index = ( i - 1 ) * v + j - 1
+            if adj_matrix[ index ]:
+                str_matrix.append(str(i) + " " + str(j) + " " + str(adj_matrix[index]))
+    return str_matrix
+
+# Return a random integer between 0 and k-1 inclusive.
+def ran( k ):
+    return random.randint(0, k-1)
+
+def random_connected_graph(v, e, w):
+    adj_matrix = [0] * v * v
+    tree = [0] * v
+    init_array(tree, v)
+    tree = permute(tree)
+
+    for i in range(1, v):
+        j = ran( i )
+        adj_matrix[ tree[ i ] * v + tree[ j ] ] = ran(w) + 1
+        adj_matrix[ tree[ j ] * v + tree[ i ] ] = ran(w) + 1
+
+    count = v - 1
+    while count < e:
+        i = ran( v )
+        j = ran( v )
+
+        if i == j:
+            continue
+
+        if i > j :
+            i, j = j, i
+
+        index = i * v + j
+        if not adj_matrix[ index ]:
+            adj_matrix[ index ] = ran(w) + 1
+            count += 1
+
+    return adj_matrix
+
+def permute(arr):
+    perm_list = list(itertools.permutations(arr))
+    return random.choice(perm_list)
+
+def init_array(arr, end):
+   for i in range(0, end):
+      arr[i] = i
+
+def random_topology(switch_number, host_number, max_bw, host_type):
+    link_number = ran(switch_number * (switch_number-1)/2)
+
+    adj_matrix = random_connected_graph(switch_number, link_number, max_bw)
+
+    net = Mininet(switch=OVSKernelSwitch, host=host_type, ipBase='10.0.0.0/8', waitConnected = True)
+    switches = [None] * switch_number
+    for i in range(0, switch_number):
+      switches[i] = net.addSwitch('s' + str(i + 1))
+
+    for i in range(0, host_number):
+        host = net.addHost('h'+ str(i + 1))
+        selected_sw = random.choice(switches)
+        net.addLink(selected_sw, host, bw=max_bw)
+
+    for i in range (1, switch_number):
+        for j in range(i+1, switch_number+1):
+            index = ( i - 1 ) * switch_number + j - 1
+            if adj_matrix[ index ]:
+                net.addLink(switches[i-1], switches[j-1], bw=adj_matrix[index])
+    return net
+
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    n = int(input("Number of switches:"))
+    h = int(input("Number of hosts:"))
+
+    max_bw = int(input("Maximum Bandwidth:"))
+    print("N=%d H=%d MaxBW=%d\n" % (n, h, max_bw))
+
+    net = random_topology(n, h, max_bw)
+    net.build()
+    CLI( net )
+    net.stop()
